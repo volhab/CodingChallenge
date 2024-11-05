7696990554847f46e42025bebfe3c10e45785a4b
Olga Balashova <olga.shadrina@gmail.com>
2024-11-05 12:28:47 -0800

    Update Solution.cs

--- CodingChallenge.FamilyTree/Solution.cs
+++ CodingChallenge.FamilyTree/Solution.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System.Collections.Generic;
 
 namespace CodingChallenge.FamilyTree
 {
@@ -6,7 +6,30 @@ namespace CodingChallenge.FamilyTree
     {
         public string GetBirthMonth(Person person, string descendantName)
         {
-            throw new NotImplementedException();
+            var list = new List<Person>();
+            CreateList(person, descendantName, ref list);
+            var result = string.Empty;
+            foreach (var item in list) 
+            { 
+                if (item.Name == descendantName)
+                {
+                    result = item.Birthday.ToString("MMMM");
+                    break;
+                }
+            }
+            return result;
+        }
+
+        private void CreateList(Person person, string descendantName, ref List<Person> list)
+        {
+            if (person != null)
+            {
+                list.Add(person);
+                foreach (var child in person.Descendants)
+                {
+                    CreateList(child, descendantName, ref list);
+                }
+            }
         }
     }
 }
