e023d20d715fbd2f6e50494b03c82a063abbf629
Olga Balashova <olga.shadrina@gmail.com>
2024-11-05 11:33:44 -0800

    Update Solution.cs

--- CodingChallenge.PirateSpeak/Solution.cs
+++ CodingChallenge.PirateSpeak/Solution.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CodingChallenge.PirateSpeak
@@ -7,7 +7,23 @@ namespace CodingChallenge.PirateSpeak
     {
         public string[] GetPossibleWords(string jumble, string[] dictionary)
         {
-            throw new NotImplementedException();
+            var result = new List<string>(); ;
+            string jumbleOrdered = GetOrdered(jumble);
+            foreach (var word in dictionary)
+            {
+                var wordOrdered = GetOrdered(word);
+                if (jumbleOrdered == wordOrdered)
+                {
+                    result.Add(word);
+                }
+            }
+            return result.ToArray();
+        }
+
+        private static string GetOrdered(string word)
+        {
+            var chars = word.ToArray().OrderBy(x => x).ToArray();
+            return new string(chars);
         }
     }
 }
