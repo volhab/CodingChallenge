df5a65b0d75ff3f3c5f21fe1dedaf78146fe4a2e
Olga Balashova <olga.shadrina@gmail.com>
2024-11-06 11:24:54 -0800

    Update Tests.cs

--- CodingChallenge.FamilyTree.Tests/Tests.cs
+++ CodingChallenge.FamilyTree.Tests/Tests.cs
@@ -1,35 +1,18 @@
 using System;
 using NUnit.Framework;
 
-
 namespace CodingChallenge.FamilyTree.Tests
 {
     [TestFixture]
     public class TreeTests
     {
-        [Test]
-        public void if_the_person_exists_in_tree_the_result_should_be_their_birthday()
+        Person tree = FamilyTreeGenerator.Make();
+       
+        [TestCase(1)]
+        [TestCase(22)]
+        [TestCase(33)]
+        public void if_the_person_exists_in_tree_the_result_should_be_their_birthday(int index)
         {
-            int index = 1;
-            var tree = FamilyTreeGenerator.Make();
-            var result = new Solution().GetBirthMonth(tree, "Name" + index);
-            Assert.AreEqual(DateTime.Now.AddDays(index - 1).ToString("MMMM"), result);
-        }
-
-        [Test]
-        public void if_the_person_exists_in_tree_the_result_should_be_their_birthday_22()
-        {
-            int index = 1;
-            var tree = FamilyTreeGenerator.Make();
-            var result = new Solution().GetBirthMonth(tree, "Name" + index);
-            Assert.AreEqual(DateTime.Now.AddDays(index - 1).ToString("MMMM"), result);
-        }
-
-        [Test]
-        public void if_the_person_exists_in_tree_the_result_should_be_their_birthday_33()
-        {
-            int index = 33;
-            var tree = FamilyTreeGenerator.Make();
             var result = new Solution().GetBirthMonth(tree, "Name" + index);
             Assert.AreEqual(DateTime.Now.AddDays(index - 1).ToString("MMMM"), result);
         }
